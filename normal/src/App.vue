89bb1748d9fb9b31b43ed8915883e332852a4f8e
BadangGit <badangva@gmail.com>
2024-01-13 13:20:52 +0900

    add font
    add google font "Inter" to html tag

--- normal/src/App.vue
+++ normal/src/App.vue
@@ -1,18 +1,37 @@
 <script lang="ts"></script>
 
 <template>
-  <nav class="mainNav">
-    <router-link to="/">physics-normal</router-link>
+  <nav class="main-nav">
+    <router-link
+      class="nav-title"
+      style="text-decoration: none; color: inherit"
+      to="/"
+    >
+      Physics-normal
+    </router-link>
   </nav>
 
   <router-view></router-view>
 </template>
 
 <style>
-.mainNav {
+* {
+  font-family: "Inter";
+}
+
+.main-nav {
   width: 100%;
   height: 77.5px;
 
   background-color: antiquewhite;
+}
+
+.nav-title {
+  display: flex;
+  text-align: center;
+  justify-content: center;
+
+  font-size: 32px;
+  font-weight: 700;
 }
 </style>
