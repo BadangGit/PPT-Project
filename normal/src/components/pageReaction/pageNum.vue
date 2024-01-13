1daa8c9df9fce670721e3dc27996b2d015ae6ba7
BadangGit <badangva@gmail.com>
2024-01-13 16:54:45 +0900

    set pageNumber to vuex

--- normal/src/components/pageReaction/pageNum.vue
+++ normal/src/components/pageReaction/pageNum.vue
@@ -1,35 +1,51 @@
 <script lang="ts" setup>
 import { useStore } from "vuex";
+import { ref } from "vue";
 
 let pageNumCounts: number = 3;
+let nowPageNum: number = 1;
+let renderCount = ref(0);
 
 const store = useStore();
 
 function shiftPage(value) {
   store.dispatch("shiftPage", value);
+  nowPageNum = store.state.nowPageNum;
+  renderCount.value += 1;
 }
 </script>
 
 <script lang="ts"></script>
 
 <template>
-  <div class="pageNumGrid">
+  <div class="pageNumGrid" :key="renderCount">
     <div
       :class="nowPageNum == pageNumCount ? 'highlight' : ''"
+      class="pageNum"
       v-for="pageNumCount in pageNumCounts"
       :key="pageNumCount"
       @click="shiftPage(pageNumCount)"
     >
       {{ pageNumCount }}
     </div>
-
-    <div>현재 페이지 {{ this.$store.state.nowPageNum }}</div>
   </div>
 </template>
 
 <style>
 .pageNumGrid {
   display: flex;
+}
+
+.pageNum {
+  cursor: pointer;
+  margin-right: 10px;
+  margin-left: 10px;
+  font-size: 32px;
+
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
 }
 
 .highlight {
