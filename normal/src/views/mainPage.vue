c44713346c9c1dc80e13355366391d38c925157f
BadangGit <badangva@gmail.com>
2024-01-13 14:08:04 +0900

    add physics situation
    2 to 18

--- normal/src/views/mainPage.vue
+++ normal/src/views/mainPage.vue
@@ -3,14 +3,30 @@ import ItemCard from "@/components/cards/itemCard.vue";
 </script>
 
 <script lang="ts">
-type categorys = {
+type Categories = {
   name: string;
   src?: string;
 };
 
-let simulCategorys: Array<categorys> = [
-  { name: "Crash" },
-  { name: "Acceleration" },
+const simulCategories: Array<Categories> = [
+  { name: "Throwing a Ball Up" },
+  { name: "Spinning a Yo-Yo" },
+  { name: "Swinging Pendulum" },
+  { name: "Pushing a Car" },
+  { name: "Blowing Bubbles" },
+  { name: "Feeling Hot or Cold" },
+  { name: "Looking in a Mirror" },
+  { name: "Turning on a Light Bulb" },
+  { name: "Playing with Magnets" },
+  { name: "Listening to Music" },
+  { name: "Running on a Treadmill" },
+  { name: "Bouncing a Ball" },
+  { name: "Glow-in-the-Dark Stars" },
+  { name: "Seeing a Rainbow" },
+  { name: "Using a Flashlight" },
+  { name: "Using a Microwave" },
+  { name: "Playing with Playdough" },
+  { name: "Seeing the Moon" },
 ];
 </script>
 
@@ -18,7 +34,7 @@ let simulCategorys: Array<categorys> = [
   <div>asdf</div>
 
   <ItemCard
-    v-for="simulCategory in simulCategorys"
+    v-for="simulCategory in simulCategories"
     :key="simulCategory.name"
     :simul-category="simulCategory"
   ></ItemCard>
