eaadddf384477cc595d43075ee11fb25e44300d7
BadangGit <badangva@gmail.com>
2024-01-16 20:59:14 +0900

    made error page

--- normal/src/router/index.ts
+++ normal/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 import mainPage from "../views/mainPage.vue";
 import simulationPage from "../views/simulationPage.vue";
+import errorPage from "../views/errorPage.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -13,6 +14,11 @@ const routes: Array<RouteRecordRaw> = [
     path: "/simulation",
     name: "simulation",
     component: simulationPage,
+  },
+
+  {
+    path: "/:catchAll(.*)",
+    component: errorPage,
   },
 ];
 
---
+++ normal/src/views/errorPage.vue
@@ -0,0 +1,5 @@
+<script lang="ts"></script>
+
+<template>
+  <div>this is Error Page</div>
+</template>
