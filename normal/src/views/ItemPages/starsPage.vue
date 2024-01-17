cdcedc20da1b53e9f0b998a71d0f5013dc20b9a8
BadangGit <badangva@gmail.com>
2024-01-17 21:08:19 +0900

    add scss loader

--- normal/src/views/ItemPages/starsPage.vue
+++ normal/src/views/ItemPages/starsPage.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="mainStarsPage">asdf</div>
+  <div class="mainStarsPage">
+    <div class="backgroundStar"></div>
+    <div class="ground"></div>
+  </div>
 </template>
 
-<style>
+<style lang="scss">
 .mainStarsPage {
   background-color: #0c090a;
   min-height: 911px;
+}
+
+.backgroundStar {
+  position: absolute;
+  background-color: white;
+
+  height: 10px;
+  width: 10px;
 }
 </style>
