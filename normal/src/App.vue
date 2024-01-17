4243d3773a014c6a44d4b9c9e942250e8b6086b2
BadangGit <badangva@gmail.com>
2024-01-17 18:51:20 +0900

    delete main-nav

--- normal/src/App.vue
+++ normal/src/App.vue
@@ -1,10 +1,6 @@
-<script lang="ts"></script>
+<script lang="ts" setup></script>
 
 <template>
-  <nav class="main-nav">
-    <router-link class="nav-title" to="/"> Physics-Normal </router-link>
-  </nav>
-
   <router-view></router-view>
 </template>
 
@@ -16,29 +12,5 @@
   -moz-user-select: none;
   -ms-user-select: none;
   user-select: none;
-}
-
-.main-nav {
-  margin: 0px;
-  width: 100%;
-  min-width: 1200px;
-  height: 60px;
-  cursor: default;
-
-  outline: solid 0.5px #cfcfcf;
-}
-
-.nav-title {
-  height: 100%;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  font-size: 32px;
-  font-weight: 700;
-  color: inherit;
-
-  text-decoration: none;
 }
 </style>
