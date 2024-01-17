c68b57a1836183936c64024246d1bc4566ac15a8
BadangGit <badangva@gmail.com>
2024-01-17 21:02:08 +0900

    basic background setup

--- normal/src/views/ItemPages/starsPage.vue
+++ normal/src/views/ItemPages/starsPage.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>this is starsPage</div>
+  <div class="mainStarsPage">asdf</div>
 </template>
+
+<style>
+.mainStarsPage {
+  background-color: #0c090a;
+  min-height: 911px;
+}
+</style>
