e05b4906d23175c18b36f62f295e27306718cc53
BadangGit <badangva@gmail.com>
2024-01-19 22:01:04 +0900

    add slide fade animation

--- normal/src/App.vue
+++ normal/src/App.vue
@@ -2,7 +2,9 @@
 
 <template>
   <div class="app">
-    <router-view></router-view>
+    <Transition name="slide-fade" mode="out-in">
+      <router-view></router-view>
+    </Transition>
   </div>
 </template>
 
@@ -23,5 +25,20 @@
 .app {
   width: 100%;
   height: 100%;
+}
+
+/* slide-animation */
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
 }
 </style>
