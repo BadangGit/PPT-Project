92c3d62ddc1daf1bc1009afe4a8255a2a481f193
BadangGit <badangva@gmail.com>
2024-01-19 22:01:41 +0900

    add animation in mainPage

--- normal/src/views/mainPage.vue
+++ normal/src/views/mainPage.vue
@@ -84,13 +84,15 @@ export default {
 
     <div class="mainItemGrid">
       <div class="cardGrid">
-        <ItemCard
-          :class="`itemCard num${simulCategory.cardNum}`"
-          v-for="simulCategory in nowPageSimulCategories"
-          :key="simulCategory.name"
-          :simul-category="simulCategory"
-        >
-        </ItemCard>
+        <transition-group name="slide-fade" mode="out-in">
+          <ItemCard
+            :class="`itemCard num${simulCategory.cardNum}`"
+            v-for="simulCategory in nowPageSimulCategories"
+            :key="simulCategory.name"
+            :simul-category="simulCategory"
+          >
+          </ItemCard>
+        </transition-group>
       </div>
       <div class="pageNumGrid">
         <div
@@ -197,5 +199,20 @@ export default {
 
 .right img {
   float: right;
+}
+
+/* fade animation */
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
 }
 </style>
