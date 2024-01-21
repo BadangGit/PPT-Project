50a0537cf7a9011071a62e7af51c24311928960e
BadangGit <badangva@gmail.com>
2024-01-21 20:30:21 +0900

    remove duplication style code

--- normal/src/views/mainPage.vue
+++ normal/src/views/mainPage.vue
@@ -216,10 +216,6 @@ export default {
 
 .arrowGrid a {
   cursor: pointer;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
 }
 
 .arrowGrid img {
