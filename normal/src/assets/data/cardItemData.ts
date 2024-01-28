96427c337d5d050c8c931598d6ee234bc140d656
BadangGit <badangva@gmail.com>
2024-01-28 21:00:17 +0900

    split card item data file

---
+++ normal/src/assets/data/cardItemData.ts
@@ -0,0 +1,24 @@
+import { Categories } from "./types";
+
+const simulCategories: Array<Categories> = [
+  { name: "Stars", cardNum: 0, subText: "When falling down?" },
+  { name: "Preparing", cardNum: 1, subText: "wait for update" },
+  { name: "Preparing", cardNum: 2, subText: "wait for update" },
+  { name: "Preparing", cardNum: 3, subText: "wait for update" },
+  { name: "Preparing", cardNum: 4, subText: "wait for update" },
+  { name: "Preparing", cardNum: 5, subText: "wait for update" },
+  { name: "Preparing", cardNum: 6, subText: "wait for update" },
+  { name: "Preparing", cardNum: 7, subText: "wait for update" },
+  { name: "Preparing", cardNum: 8, subText: "wait for update" },
+  { name: "Preparing", cardNum: 9, subText: "wait for update" },
+  { name: "Preparing", cardNum: 10, subText: "wait for update" },
+  { name: "Preparing", cardNum: 11, subText: "wait for update" },
+  { name: "Preparing", cardNum: 12, subText: "wait for update" },
+  { name: "Preparing", cardNum: 13, subText: "wait for update" },
+  { name: "Preparing", cardNum: 14, subText: "wait for update" },
+  { name: "Preparing", cardNum: 15, subText: "wait for update" },
+  { name: "Preparing", cardNum: 16, subText: "wait for update" },
+  { name: "Preparing", cardNum: 17, subText: "wait for update" },
+];
+
+export { simulCategories };
--- normal/src/views/mainPage.vue
+++ normal/src/views/mainPage.vue
@@ -3,32 +3,12 @@ import ItemCard from "@/components/cards/itemCard.vue";
 import DarkModeButton from "@/components/buttons/darkModeButton.vue";
 
 import { Categories } from "@/assets/data/types";
+import { simulCategories } from "@/assets/data/cardItemData";
 
 import { useStore } from "vuex";
 import { ref, watch } from "vue";
 
 const store = useStore();
-
-const simulCategories: Array<Categories> = [
-  { name: "Stars", cardNum: 0, subText: "When falling down?" },
-  { name: "Preparing", cardNum: 1, subText: "wait for update" },
-  { name: "Preparing", cardNum: 2, subText: "wait for update" },
-  { name: "Preparing", cardNum: 3, subText: "wait for update" },
-  { name: "Preparing", cardNum: 4, subText: "wait for update" },
-  { name: "Preparing", cardNum: 5, subText: "wait for update" },
-  { name: "Preparing", cardNum: 6, subText: "wait for update" },
-  { name: "Preparing", cardNum: 7, subText: "wait for update" },
-  { name: "Preparing", cardNum: 8, subText: "wait for update" },
-  { name: "Preparing", cardNum: 9, subText: "wait for update" },
-  { name: "Preparing", cardNum: 10, subText: "wait for update" },
-  { name: "Preparing", cardNum: 11, subText: "wait for update" },
-  { name: "Preparing", cardNum: 12, subText: "wait for update" },
-  { name: "Preparing", cardNum: 13, subText: "wait for update" },
-  { name: "Preparing", cardNum: 14, subText: "wait for update" },
-  { name: "Preparing", cardNum: 15, subText: "wait for update" },
-  { name: "Preparing", cardNum: 16, subText: "wait for update" },
-  { name: "Preparing", cardNum: 17, subText: "wait for update" },
-];
 
 let nowPageSimulCategories: Array<Categories> = [];
 let pageNumCounts: number = 3;
