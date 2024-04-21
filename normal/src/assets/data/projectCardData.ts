2b90d542a7f8850823f83f14aee4ecf4b34284ba
BadangGit <badangva@gmail.com>
2024-04-21 12:29:15 +0900

    add dynamic cursor

--- normal/src/assets/data/projectCardData.ts
+++ normal/src/assets/data/projectCardData.ts
@@ -1,9 +1,24 @@
 import { projectCardContentsType } from "@/assets/data/types/projectCard";
 
 const projectCardList: Array<projectCardContentsType> = [
-  { title: "Stars", subtitle: "When falling down", num: 0 },
-  { title: "AI", subtitle: "Isn't it incredible?", num: 1 },
-  { title: "Preparing", subtitle: "wait for update", num: 2 },
+  {
+    title: "Stars",
+    subtitle: "When falling down",
+    num: 0,
+    icon: "http://localhost:8080/icons/stars.png",
+  },
+  {
+    title: "AI",
+    subtitle: "Isn't it incredible?",
+    num: 1,
+    icon: "http://localhost:8080/icons/AI.png",
+  },
+  {
+    title: "Preparing",
+    subtitle: "wait for update",
+    num: 2,
+    icon: "http://localhost:8080/icons/Preparing.png",
+  },
   { title: "Preparing", subtitle: "wait for update", num: 3 },
   { title: "Preparing", subtitle: "wait for update", num: 4 },
   { title: "Preparing", subtitle: "wait for update", num: 5 },
--- normal/src/assets/data/types/projectCard.d.ts
+++ normal/src/assets/data/types/projectCard.d.ts
@@ -2,4 +2,5 @@ export type projectCardContentsType = {
   title: string;
   subtitle: string;
   num: number;
+  icon?: string;
 };
