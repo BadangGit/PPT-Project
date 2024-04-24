4d9d72843a1e1e8dd34cf774cc553da36937a246
BadangGit <badangva@gmail.com>
2024-04-24 21:51:46 +0900

    starCards grid setup

--- normal/src/components/cards/starCard.vue
+++ normal/src/components/cards/starCard.vue
@@ -1,7 +1,26 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="starCardGrid">this is star card</div>
+  <div class="starCard">this is star card</div>
 </template>
 
-<style></style>
+<style>
+.starCard {
+  height: 360px;
+  width: 320px;
+
+  margin: auto;
+  margin-top: 80px;
+
+  background-color: white;
+  border-radius: 20px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+
+  transition: box-shadow;
+  transition-duration: 0.2s;
+}
+
+.starCard:hover {
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
+</style>
--- normal/src/views/ItemPages/starsPage.vue
+++ normal/src/views/ItemPages/starsPage.vue
@@ -1,20 +1,43 @@
 <script setup lang="ts">
 import StarCard from "@/components/cards/starCard.vue";
+
+const items = 12;
 </script>
 
 <template>
-  <div class="starPageGrid">
-    <div class="cards">
-      <StarCard></StarCard>
+  <div class="background">
+    <div class="starPageGrid">
+      <div class="starCardGrid">
+        <StarCard v-for="item in items" :key="item"> </StarCard>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
-.starPageGrid {
-  width: 100vw;
-  height: 100vh;
+.background {
+  background-color: #f3f3f3;
+}
 
-  background-image: linear-gradient(to top, #ce9dc1 0%, #7696c9 100%);
+.starPageGrid {
+  width: 1400px;
+  height: auto;
+  margin: auto;
+
+  background-image: repeating-linear-gradient(
+    #f1c5e6 0%,
+    #83a4da 50%,
+    #f1c5e6 100%
+  );
+}
+
+.starCardGrid {
+  width: 1200px;
+  height: 100%;
+
+  margin: auto;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
 }
 </style>
