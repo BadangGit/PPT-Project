b5f535cd1c625fffc735341678e60e06df799c24
BadangGit <badangva@gmail.com>
2024-04-25 20:16:46 +0900

    add starCard content

--- normal/src/components/cards/starCard.vue
+++ normal/src/components/cards/starCard.vue
@@ -1,20 +1,26 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="starCard"></div>
+  <div class="starCard">
+    <div class="starCardContent"></div>
+  </div>
 </template>
 
 <style>
 .starCard {
   height: 400px;
   width: 380px;
+  border-radius: 20px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   margin: auto;
   margin-top: 60px;
   margin-bottom: 60px;
 
   background-color: white;
-  border-radius: 20px;
   box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 
   transition: box-shadow;
@@ -23,5 +29,13 @@
 
 .starCard:hover {
   box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
+
+.starCardContent {
+  height: 360px;
+  width: 340px;
+  border-radius: 20px;
+
+  background-color: white;
 }
 </style>
