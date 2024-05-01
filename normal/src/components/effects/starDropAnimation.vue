bfb7726cdc93b507dedc2dc1044e9d9739e8bf6c
BadangGit <badangva@gmail.com>
2024-05-01 21:58:09 +0900

    reset setups

--- normal/src/components/effects/starDropAnimation.vue
+++ normal/src/components/effects/starDropAnimation.vue
@@ -1,17 +1,28 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="starDropGrid">Star Drop Animation Component</div>
+  <div class="starDropGrid">
+    <div class="stars"></div>
+  </div>
 </template>
 
-<style>
+<style lang="scss">
 .starDropGrid {
   height: var(--card-height);
   width: var(--card-width);
   border-radius: var(--card-border-radius);
 
   position: absolute;
+  display: flex;
 
   pointer-events: none;
+}
+
+.stars {
+  height: 3px;
+  width: 3px;
+  border-radius: 1.5px;
+
+  background-color: white;
 }
 </style>
