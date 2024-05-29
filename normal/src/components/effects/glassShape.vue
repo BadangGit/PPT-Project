64990a6dbe3ca818cb7da12468847d2e278bb047
BadangGit <badangva@gmail.com>
2024-05-29 21:56:49 +0900

    set glass design

--- normal/src/components/effects/glassShape.vue
+++ normal/src/components/effects/glassShape.vue
@@ -1,9 +1,14 @@
 <script setup>
 import { ref } from "vue";
-import { getRandomPos, getRandomSize } from "@/assets/function/getRandomData";
+import {
+  getRandomPos,
+  getRandomSize,
+  getRandomColor,
+} from "@/assets/function/getRandomData";
 
 const pos = ref(getRandomPos(screen.width, screen.height));
 const size = ref(getRandomSize(150, 250));
+const color = ref(getRandomColor());
 </script>
 
 <template>
@@ -15,11 +20,17 @@ const size = ref(getRandomSize(150, 250));
   width: v-bind("size.width");
   height: v-bind("size.height");
 
-  background-color: red;
+  opacity: 0.3;
+
+  background-color: v-bind("color");
 
   position: absolute;
 
   left: v-bind("pos.left");
   top: v-bind("pos.top");
+
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(7.9px);
+  border: 1px solid rgba(255, 255, 255, 1);
 }
 </style>
