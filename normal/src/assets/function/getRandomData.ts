ddc2eb63abb6635a258b0b25aabaa84552b3df9f
BadangGit <badangva@gmail.com>
2024-05-29 21:39:11 +0900

    add getRandomColor function to file

---
+++ normal/src/assets/function/getRandomData.ts
@@ -0,0 +1,34 @@
+
+export function getRandomPos(width: number, height: number) {
+  let overflowSpace = Math.random() * 200;
+  let left: number = 0;
+  let top: number = 0;
+
+  if (Math.random() < 0.5) {
+    left = Math.floor(Math.random() * width - overflowSpace);
+    top = Math.floor(Math.random() * height - overflowSpace);
+  } else {
+    left = Math.floor(Math.random() * width + overflowSpace);
+    top = Math.floor(Math.random() * height + overflowSpace);
+  }
+
+  let pos = {
+    left: numberToPx(left),
+    top: numberToPx(top),
+  };
+
+  return pos;
+}
+
+export function getRandomSize(max: number, min: number) {
+  let width = Math.floor(Math.random() * (max - min)) + min;
+  let height = Math.floor(Math.random() * (max - min)) + min;
+
+  let size = {
+    width: numberToPx(width),
+    height: numberToPx(height),
+  };
+
+  return size;
+}
+
--- normal/src/views/ItemPages/starsPage.vue
+++ normal/src/views/ItemPages/starsPage.vue
@@ -1,15 +1,7 @@
 <script setup lang="ts">
 import StarCard from "@/components/cards/starCard.vue";
 import { gradientColor } from "@/assets/data/types/starCard";
-
-function getRandomColor() {
-  let randomColor = "";
-  for (let i = 0; i < 6; i++) {
-    randomColor += Math.round(Math.random() * 0xf).toString(16);
-  }
-
-  return "#" + randomColor;
-}
+import { getRandomColor } from "@/assets/function/getRandomData";
 
 let items: number = 12;
 let backgroundColorStyle = ["#f1c5e6", "#becde7"];
