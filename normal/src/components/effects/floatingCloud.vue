0e3bc659e2a6862c4bdd374ff8b294ab900da22f
BadangGit <badangva@gmail.com>
2024-06-23 23:16:14 +0900

    theme park basic set

---
+++ normal/src/components/effects/floatingCloud.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="cloud">
+    <div class="underBall"></div>
+  </div>
+</template>
+
+<style scoped>
+.cloud {
+  border: 2px solid black;
+}
+
+.underBall {
+  width: 200px;
+  height: 80px;
+
+  border-radius: 40px;
+
+  background: var(--cloud-gradient-color);
+}
+</style>
--- normal/src/views/ItemPages/themeParkPage.vue
+++ normal/src/views/ItemPages/themeParkPage.vue
@@ -1,7 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import floatingCloud from "@/components/effects/floatingCloud.vue";
+</script>
 
 <template>
-  <div>adsf</div>
+  <div class="parkGrid">
+    <div class="sky">
+      <floatingCloud></floatingCloud>
+    </div>
+    <div class="ground"></div>
+  </div>
 </template>
 
-<style></style>
+<style scoped>
+.parkGrid {
+  --sky-gradient-color: linear-gradient(#87ceeb, white);
+  --cloud-gradient-color: linear-gradient(white, #bfe0ec);
+}
+
+.parkGrid {
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+}
+
+.sky {
+  width: 100%;
+  height: 100%;
+
+  background: var(--sky-gradient-color);
+}
+
+.ground {
+  width: 100%;
+  height: 100px;
+
+  background-color: #b3907a;
+  position: absolute;
+  bottom: 0px;
+}
+</style>
