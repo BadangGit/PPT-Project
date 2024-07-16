8d57e5069a2b805cc0b5b338725a9591ae4a791e
BadangGit <badangva@gmail.com>
2024-07-16 21:58:01 +0900

    delete animation

--- normal/src/components/buttons/baseButton.vue
+++ normal/src/components/buttons/baseButton.vue
@@ -11,14 +11,12 @@
   width: 100%;
   height: 100%;
 
-  border-radius: 15%;
+  border-radius: 35%;
 
   display: flex;
 
   justify-content: center;
   align-items: center;
-
-  background-color: rgb(84, 158, 255);
 }
 
 .buttonGrid img {
@@ -26,36 +24,5 @@
   height: 35px;
   margin-left: 2px;
   margin-top: 2px;
-
-  filter: invert();
-}
-
-.buttonGrid:hover img {
-  animation-name: arrowMove;
-  animation-duration: 0.8s;
-  animation-timing-function: linear;
-  animation-iteration-count: infinite;
-}
-
-@keyframes arrowMove {
-  0% {
-    margin-left: 2px;
-  }
-
-  25% {
-    margin-left: 10px;
-  }
-
-  50% {
-    margin-left: 2px;
-  }
-
-  75% {
-    margin-left: -2px;
-  }
-
-  100% {
-    margin-left: 2px;
-  }
 }
 </style>
